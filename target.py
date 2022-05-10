2e8cfed1004c25b0ddd4ba7559796e9bf6b35f5f
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-05-10 16:06:56 +0200

    Added class Target

---
+++ target.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Sprite
+
+# A class for showing on the screen a target
+class Target(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        self.image = pygame.image.load("Spaceship_shooter/images/silly.png")  # image of the target
+        self.rect = self.image.get_rect()  # setting the image as a rectangle
+
+        self.rect.x = self.rect.width  # space between targets equal to the width of one target
+        self.rect.y = self.rect.height # space above targets equal to the height of one target
+        self.x = float(self.rect.x)  # tracking the horizontal position of a target
+
+    # Information for the program if an target has touched an edge of the display window
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    # Deciding which way to move the target
+    def update(self):
+        self.x += (self.settings.target_speed * self.settings.fleet_direction)
+        self.rect.x = self.x  # updating its position
+
